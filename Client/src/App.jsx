b4f818fa2e1b6755cf7eaea9fd7c9ed1a6e4a10f
“JorgeAlfredoLozano” <117942848+JorgeAlfredoLozano@users.noreply.github.com>
2023-11-28 20:47:44 -0300

    con autenticacion por token

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import axios from 'axios';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firebaseConfig, setFirebaseConfig] = useState(null);
 
+
+  const signInWithGoogle = async () => {
+    try {
+      // Obtener la configuración de Firebase desde el servidor utilizando Axios
+      const response = await axios.get('http://localhost:5000/api/firebase-config');
+
+      if (!response.data) {
+        throw new Error('Empty response from server');
+      }
+
+      // Almacena la configuración en el estado
+      setFirebaseConfig(response.data);
+
+      // Inicializar Firebase en el cliente con la configuración obtenida
+      const app = initializeApp(response.data);
+      const auth = getAuth(app);
+
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+
+      // Usuario autenticado con éxito
+      const user = result.user;
+      console.log('Usuario autenticado:', user);
+      console.log('PRUEBA USER TOKEN: ',user.accessToken) //propiedad accesToken
+      const token = await user.getIdToken(); // metodo para pedir el token
+      console.log('Token de usuario logueado: ', token);
+
+
+
+      // Hacer una solicitud GET a la ruta /contacto con el token
+      const contactoResponse = await axios.get('http://localhost:5000/api/contacto', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log('Respuesta de la ruta /contacto:', contactoResponse.data);
+    } catch (error) {
+      console.error('Error signing in with Google:', error.message);
+    }
+  };
+
+  const contactohandler = async () => {
+    // Hacer una solicitud GET a la ruta /contacto con el token
+    const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjBiYmQyOTllODU2MmU3MmYyZThkN2YwMTliYTdiZjAxMWFlZjU1Y2EiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiSm9yZ2UgTG96YW5vIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0pqLURhNHpaQ3U0b0hCSDJiSjVPYkw5TTZvNnBicmEzYm0zSHlrUUUzUzJ1WHQ9czk2LWMiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZXZlbnR3YXZlLWFyIiwiYXVkIjoiZXZlbnR3YXZlLWFyIiwiYXV0aF90aW1lIjoxNzAxMjEyODIwLCJ1c2VyX2lkIjoiaGZPSk85TFI3aVdBYzg5THJIcjBUeTZFcjFFMiIsInN1YiI6ImhmT0pPOUxSN2lXQWM4OUxySHIwVHk2RXIxRTIiLCJpYXQiOjE3MDEyMTI4MjAsImV4cCI6MTcwMTIxNjQyMCwiZW1haWwiOiJwdW50b25ldGF6dWxAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZ29vZ2xlLmNvbSI6WyIxMDg5MTY3NzAzNzEwOTQ1NjczMzAiXSwiZW1haWwiOlsicHVudG9uZXRhenVsQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Imdvb2dsZS5jb20ifX0.jhC_xeeVUlY8gGhAes8kEsoBlyylvyVBFPBO7YCo3JOuTWiACs-YZ1noUGISfA3iY4SRC8Co7JTPGi41-C14I-xhqbc_ullN9uvwyW4-KLdpKszb5D2B8V0zEVQ6guPk8ryeXhTOzBmF9eJhYDoNtSzhtRLBWT6Du1_zeBUFqqLiJELjJrv1RUakYU6_hO90L9Zhy0Evv9GkSYpupN2M4b-AFZrOQY5T8iMlzr8270o5F_XL51l6B83Tw_IhVTVniD7O2fk_sOSR9_fHSlksGW1G3l0pYtu8XScF1PheK_RBoCRvslfnHof0v_oaFFjx14Rzx3FwHzEF1DA9RxgACQ'
+    try {
+      const contactoResponse = await axios.get('http://localhost:5000/api/contacto', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log('Respuesta de la ruta /contacto:', contactoResponse.data);
+    } catch (error) {
+      console.error('Error signing in with Google:', error.message);
+    }
+    
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>EventWave ATR</h1>
+      <button onClick={signInWithGoogle}>Iniciar sesión con Google</button>
+      <button onClick={contactohandler}>contacto</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,54 @@
-// client/server.js
-
 const express = require('express');
-const path = require('path');
+const admin = require('firebase-admin');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-// Servir archivos estáticos desde la carpeta 'dist'
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Todas las rutas que no coincidan con un archivo en 'dist' serán redirigidas al index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// Configuración de Firebase Admin SDK
+const serviceAccount = require('../Server/eventwave-ar-firebase-adminsdk-i8suk-1b96ec4c0c.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
-app.listen(port, () => {
-  console.log(`Servidor Express escuchando en el puerto ${port}`);
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+}));
+
+// Middleware para verificar el token en cada solicitud
+const authenticateToken = async (req, res, next) => {
+  try {
+    const idToken = req.header('Authorization').split(' ')[1];
+    await admin.auth().verifyIdToken(idToken);
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+// Ruta para obtener la configuración de Firebase
+app.get('/api/firebase-config', (req, res) => {
+  const config = {
+    apiKey: "AIzaSyDSP-uPfjsK1HyyIZu7bggsRRowAdtaUpg",
+    authDomain: "eventwave-ar.firebaseapp.com",
+    projectId: "eventwave-ar",
+    storageBucket: "eventwave-ar.appspot.com",
+    messagingSenderId: "757346690931",
+    appId: "1:757346690931:web:caecc58aa042b491b94706",
+    measurementId: "G-2QKZHDXHJC"
+  };
+
+  res.json(config);
+});
+
+// Ruta protegida por token
+app.get('/api/contacto', authenticateToken, (req, res) => {
+  // Lógica para manejar la solicitud protegida
+  res.json({ telefono: '99999999',
+             email: 'email@email.com' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
